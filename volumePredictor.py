628d383674845619aed7803f1d8bd7b24227c42d
DanDaeKim <131207269+DanDaeKim@users.noreply.github.com>
2025-01-05 22:11:21 -0800

    Create volumePredictor.py

---
+++ volumePredictor.py
@@ -0,0 +1,2 @@
+# Will be training a model to predict volume based on news headlines/ weighted words and tickers from bloomberg's API 
+# Will have to investigate if there are more recent ways to access information as well their corresponding sentiment
